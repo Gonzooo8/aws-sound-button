273a4fd913055be0f3b3d5ad07b0131b07408655
Gonzooo8 <198449065+Gonzooo8@users.noreply.github.com>
2026-07-22 21:02:10 +0900

    Add EC2 IAM role and instance profile

--- terraform/main.tf
+++ terraform/main.tf
@@ -76,3 +76,32 @@ resource "aws_security_group" "web" {
     Name = "${var.project_name}-web-sg"
   }
 }
+
+resource "aws_iam_role" "ec2" {
+  name = "${var.project_name}-ec2-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Effect = "Allow"
+
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+
+  tags = {
+    Name = "${var.project_name}-ec2-role"
+  }
+}
+
+resource "aws_iam_instance_profile" "ec2" {
+  name = "${var.project_name}-ec2-instance-profile"
+  role = aws_iam_role.ec2.name
+}
