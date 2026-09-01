6d8a69f48eb6e3fca67ce8e3f06db39bc157b9e4
Gonzooo8 <198449065+Gonzooo8@users.noreply.github.com>
2026-09-01 21:58:59 +0900

    Migrate Terraform state to S3 remote backend

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,6 +1,13 @@
 terraform {
   required_version = "~> 1.15.8"
 
+  backend "s3" {
+    bucket       = "aws-sound-button-tfstate-392789867247"
+    key          = "aws-sound-button/terraform.tfstate"
+    region       = "ap-northeast-1"
+    use_lockfile = true
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
